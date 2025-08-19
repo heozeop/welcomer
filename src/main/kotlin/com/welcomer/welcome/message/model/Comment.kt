9b35d5528352620988ed34d065af1cce2fc6e82d
crispy <dltmdcks702@cau.ac.kr>
2025-08-19 09:56:40 +0900

    feat: add comment

---
+++ src/main/kotlin/com/welcomer/welcome/message/model/Comment.kt
@@ -0,0 +1,34 @@
+package com.welcomer.welcome.message.model
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.timestamp
+import java.time.LocalDateTime
+
+object Comments : Table("comments") {
+    val id = uinteger("id").autoIncrement()
+    val messageId = uinteger("message_id").references(Messages.id)
+    val author = varchar("author", 255)
+    val content = text("content")
+    val createdAt = timestamp("created_at")
+    val updatedAt = timestamp("updated_at")
+
+    override val primaryKey = PrimaryKey(id)
+}
+
+data class Comment(
+    val id: UInt?,
+    val messageId: UInt,
+    val author: String,
+    val content: String,
+    val createdAt: LocalDateTime?,
+    val updatedAt: LocalDateTime?
+) {
+    constructor(messageId: UInt, author: String, content: String) : this(
+        null,
+        messageId,
+        author,
+        content,
+        LocalDateTime.now(),
+        LocalDateTime.now()
+    )
+}
